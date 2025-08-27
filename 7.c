dc810c2f3cc8b2d09a31cf805292109b094b5b9a
Henilsavaliya501 <henilsavaliya500@gmail.com>
2025-08-27 13:44:10 +0530

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+     int min,hours;
+     printf("Enter Minutes : ");
+     scanf("%d", &min);
+     min=hours/60;
+     printf("%d hours",hours);
+     return 0;
+}
