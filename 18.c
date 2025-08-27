33457fe50f5223e1af8f94de68c8dfc43e433913
Henilsavaliya501 <henilsavaliya500@gmail.com>
2025-08-27 15:09:04 +0530

    Add files via upload

---
+++ 18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+  float lnth, wdth, perimeter, area;
+  printf("Enter the length of the Rectangle:\n");
+  scanf("%f", & lnth);
+  printf("Enter the width of the Rectangle:\n");
+  scanf("%f", & wdth);
+  perimeter = 2 * (lnth + wdth);
+  printf("Perimeter of the Rectangle: %f\n", perimeter);
+  area = lnth * wdth;
+  printf("Area of the Rectangle: %f\n", area);
+}
